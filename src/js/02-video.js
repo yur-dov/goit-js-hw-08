d30024883a01973feabeeaded938ebbaddeeb4d3
Yurii <Rapacta@gmail.com>
2022-05-24 18:27:33 +0200

    task 2 comleted

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
