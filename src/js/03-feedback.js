92db42a98cae461c39b2762b2bb36d06c4917fd7
Yurii <Rapacta@gmail.com>
2022-05-24 18:31:23 +0200

    task 3 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle'
+const STORAGE_KEY = 'feedback-form-state'
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSabmit);
+refs.form.addEventListener('input', throttle(onFormSubmit, 500));
+
+const formData = {} 
+
+function onFormSubmit(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+    console.log(formData);
+}
+
+function onFormSabmit(event) {
+    event.preventDefault()
+
+    if(refs.input.value === '' || refs.textarea.value === ''){
+        return alert('all text fields must be filled')
+    }
+
+    console.log(formData);
+    event.currentTarget.reset()
+    localStorage.removeItem(STORAGE_KEY)
+}
+populateTextarea()
+function populateTextarea() {
+    const saveMessege = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    refs.textarea.value = saveMessege.message
+    refs.input.value = saveMessege.email
+}
